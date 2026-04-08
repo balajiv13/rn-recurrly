8ae21031fa536cbcfb4066c2bc337fd0eb9ae1af
balajiv13 <balajievil13@gmail.com>
2026-04-08 08:27:18 +0530

    implemented the eslint with prettiers in project

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,25 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['node_modules', '.expo', 'dist/*', 'web-build'],
+  },
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
   },
 ]);
