c64ccb347dffe73a1333ba5ab83f3439f02eaee3
balajiv13 <balajievil13@gmail.com>
2026-04-08 08:26:16 +0530

    implemented fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,8 @@
-import "@/global.css";
-import { Link } from "expo-router";
-import { styled } from "nativewind";
-import { Text } from "react-native";
-import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import '@/global.css';
+import { Link } from 'expo-router';
+import { styled } from 'nativewind';
+import { Text } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
 
 const SafeAreaView = styled(RNSafeAreaView);
 
@@ -19,16 +19,26 @@ export default function App() {
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
       {/* // <View className="flex-1 items-center justify-center bg-background"> */}
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="./Onboarding" className="mt-4 px-4 py-2 bg-primary rounded text-white">Go to Onboarding</Link>
-      <Link href="./(auth)/sign-in" className="mt-4 px-4 py-2 bg-primary rounded text-white">Already Have an Account</Link>
-      <Link href="./(auth)/sign-up" className="mt-4 px-4 py-2 bg-primary rounded text-white">Create Account</Link>
-
-
-      <Link href="./subscriptions/spotify">Spotify Subscription</Link>
-      <Link href={{ pathname: "./subscriptions/[id]", params: { id: "claude-pro" } }}>Clude Max Subscription</Link>
+      <Text className="text-7xl font-sans-extrabold">Home</Text>
+      <Text className="text-7xl font-bold">Home</Text>
+      <Link
+        href="./Onboarding"
+        className="mt-4 px-4 py-2 bg-primary rounded font-sans-bold text-white"
+      >
+        Go to Onboarding
+      </Link>
+      <Link
+        href="./(auth)/sign-in"
+        className="mt-4 px-4 py-2 bg-primary rounded font-sans-bold text-white"
+      >
+        Already Have an Account
+      </Link>
+      <Link
+        href="./(auth)/sign-up"
+        className="mt-4 px-4 py-2 bg-primary rounded font-sans-bold text-white"
+      >
+        Create Account
+      </Link>
     </SafeAreaView>
     // </View>
   );
